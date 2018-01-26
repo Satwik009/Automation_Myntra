bd56c59b3d1ac8a9c60bb72f28d4b0ca49f90d6c
Satwik009 <ssatwik77@gmail.com>
2018-01-26 23:54:11 +0530

    new test changes

--- com.Automation.Project/src/main/java/com/Test/Base/Base.java
+++ com.Automation.Project/src/main/java/com/Test/Base/Base.java
@@ -25,7 +25,8 @@ import com.Test.Utils.Utils;
 
 public class Base {
 	  public static  WebDriver driver;
-  	
+  	// testing git 
+	  
   	@BeforeTest
 	public  static void launchApp() throws IOException
 	{
